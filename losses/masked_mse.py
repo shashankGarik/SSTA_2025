8b9a610ef1c73092feb50ccec88e2ffd792dc7f5
shashank.garikipati <shashank7g@gmail.com>
2025-04-11 17:03:01 -0400

    added masked_mse_loss

---
+++ losses/masked_mse.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn.functional as F
+
+def masked_mse(output, gt):
+    gt_mask = (gt[...,0]!=1).float()
+    bce = F.binary_cross_entropy_with_logits(output[...,0],gt_mask)
+
+    mask = gt_mask.bool()
+
+    if mask.sum() != 0:
+        masked_t2no_pred, masked_t2nd_pred  = output[...,1][mask], output[...,2][mask]
+        masked_t2no_gt, masked_t2nd_gt      = gt[...,0][mask], gt[...,1][mask]
+
+        mse_t2no = F.mse_loss(masked_t2no_pred, masked_t2no_gt)
+        mse_t2nd = F.mse_loss(masked_t2nd_pred, masked_t2nd_gt)
+    else:
+        mse_t2no = torch.tensor(0.0, device=output.device)
+        mse_t2nd = torch.tensor(0.0, device=output.device)
+
+    return bce + mse_t2no + mse_t2nd
